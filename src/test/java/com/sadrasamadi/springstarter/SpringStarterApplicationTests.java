9ecfc374bf3acd15dcc97c30e7e745089d2539af
Sadra Samadi <sadra.smd@gmail.com>
2021-05-25 10:13:24 +0430

    initial commit

---
+++ src/test/java/com/sadrasamadi/springstarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadrasamadi.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
